ea314b4a8293bd9aa06b0aebafcf505c5e60564d
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2023-08-23 15:26:14 -0400

    Update StandUp.py

--- toast-notification/StandUp.py
+++ toast-notification/StandUp.py
@@ -1,1 +1,12 @@
+from win10toast import *
+import time
 
+toaster = ToastNotifier()
+
+# time in seconds between notifications (default = 20min = 1200.0)
+interval = 1200.0
+
+startTime = time.time()
+while True:
+    toaster.show_toast("Stand up", "Look at something 20 feet away for 20 seconds", duration = 5)
+    time.sleep(interval - ((time.time()-startTime)%interval))
